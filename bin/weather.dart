7f2cfdb224935ad9e16b9c74df9f89bc6ea2311a
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-01-05 22:12:41 -0700

    Remove unused code

--- bin/weather.dart
+++ bin/weather.dart
@@ -1,7 +1,6 @@
 import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) async {
-  // var url = Uri.parse('http://v2.wttr.in/moon?An0');
   var url = Uri.parse('http://v2.wttr.in/?An0');
   var response = await http.get(url);
 
